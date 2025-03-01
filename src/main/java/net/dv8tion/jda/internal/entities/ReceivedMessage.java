a0be6a32c24e87d4ea7fb5f55656ea45f57eb1ec
freya02 <41875020+freya022@users.noreply.github.com>
2025-03-01 23:36:55 +0100

    Introduce new methods to explicitly set V2 and V1 components
    - Added `[action]ComponentTree` for V2 components
    - Brought back `[action]Components` for V1 components, deprecated
    - Added `[action]ActionRows` for V1 components
    - Got rid of `(add/set)ActionRow(ActionRowChildComponent)` as it accepts V2 components, while `setActionRows` only accepts V1

--- src/main/java/net/dv8tion/jda/internal/entities/ReceivedMessage.java
+++ src/main/java/net/dv8tion/jda/internal/entities/ReceivedMessage.java
@@ -635,6 +635,12 @@ public class ReceivedMessage implements Message
     }
 
     @Override
+    public boolean isUsingComponentsV2()
+    {
+        return (this.flags & MessageFlag.IS_COMPONENTS_V2.getValue()) > 0;
+    }
+
+    @Override
     public MessagePoll getPoll()
     {
         checkIntent();
@@ -756,10 +762,10 @@ public class ReceivedMessage implements Message
 
     @Nonnull
     @Override
-    public MessageEditAction editMessageComponents(@Nonnull Collection<? extends MessageTopLevelComponent> components)
+    public MessageEditAction editMessageComponentTree(@Nonnull Collection<? extends MessageTopLevelComponent> components)
     {
         MessageEditActionImpl action = editRequest();
-        action.setComponents(components);
+        action.setComponentTree(components);
 
         if (isWebhookRequest())
             return action.withHook(webhook);
